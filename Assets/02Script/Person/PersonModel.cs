997186a32ccd2a4a169dd515e5229862eb7b3a38
DESKTOP-UGTVCU8\gkdlz <gkdlzhtk@vr-jamong.com>
2021-10-19 15:39:21 +0900

    animation walkble state update

--- Assets/02Script/Person/PersonModel.cs
+++ Assets/02Script/Person/PersonModel.cs
@@ -112,7 +112,7 @@ public class PersonModel : MonoBehaviour
 
     public void SetToWalkAnimation()
     {
-        var isStateWasIdle = animator.GetBool("ShouldDoIdle") || animator.GetBool("LookAround");
+        var isStateWasIdle = animator.GetBool("ShouldDoIdle") || animator.GetBool("LookAround") || NavMeshAgent.velocity != Vector3.zero;
         animator.SetInteger("SittingLevel", 0);
         animator.SetBool("LookAround", false);
         animator.SetBool("ShouldTurn", false);
